a2e33ec9fd5b6e31360b11e3dd080dc90310c208
Watermelon <1050364782@qq.com>
2020-01-11 23:37:30 +0800

    MyBlog back-end v1.2.2 des:web structure is completing,front-end files are input

---
+++ myblog/target/classes/static/js/index.js
@@ -0,0 +1,107 @@
+$(function () {
+    // 设置导航栏按钮在移动端时点击展示导航栏item，再点击收回item
+    $('.menu.toggle').click(function () {
+        $('.menu.item').toggleClass('mobile-hide');
+    });
+});
+
+/****************************blog page functions******************************/
+
+$(function () {
+    $("#payButton").popup({
+        popup: $(".planar.popup"),
+        on: "click",
+        position: 'bottom center'
+    });
+
+    tocbot.init({
+        tocSelector: '.js-toc',
+        contentSelector: '.js-toc-content',
+        headingSelector: 'h1, h2, h3',
+    });
+
+    $("#categoryButton").popup({
+        popup: $(".toc-container.popup"),
+        on: "click",
+        position: 'left center'
+    });
+
+    $("#qrcodeButton").popup({
+        popup: $("#qrcode"),
+        on: "click",
+        position: "left center"
+    });
+
+    var qrcode = new QRCode("qrcode", {
+        text: "file:///D:/Programs/MyBlog/blog-front-end/blog.html",
+        width: 120,
+        height: 120,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
+    });
+
+    $("#up").click(function () {
+        $(window).scrollTo(0, 500);
+    });
+
+    var waypoint = new Waypoint({
+        element: document.getElementById('center'),
+        handler: function (direction) {
+            if (direction == "down") {
+                $("#v-toolbar").show(200);
+            } else {
+                $("#v-toolbar").hide(500);
+            }
+        }
+    })
+});
+
+/****************************about page functions******************************/
+
+$(function () {
+    $(".qq").popup();
+
+    $(".github").popup();
+
+    $(".wechat").popup({
+        popup: $(".wechat-planar.popup"),
+        on: "hover",
+        position: "bottom center"
+    });
+
+});
+
+/****************************admin page functions******************************/
+$(function () {
+    $(".ui .dropdown").dropdown({
+        on: 'hover'
+    });
+});
+
+/****************************publish page functions******************************/
+$(function () {
+    $(".ui .form").form({
+        fields: {
+            title: {
+                identifier: 'title',
+                rules: [{
+                    type: 'empty',
+                    prompt: '标题：请输入博客标题'
+                }]
+            }
+        }
+    });
+
+    // 页面插件markdown集成部分js
+    var contentEditor;
+    $(function () {
+        contentEditor = editormd("md-content", {
+            width: "90%",
+            height: 640,
+            syncScrolling: "single",
+            path: "../static/lib/editormd/lib/"
+        });
+    });
+});
+
