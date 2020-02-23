0ba7b559343bd9e4d4ed387b8ee48ca905f194ac
Watermelon <1050364782@qq.com>
2020-02-23 21:48:34 +0800

    MyBlog back-end v1.9.9 des:title icon optimized

--- myblog/src/main/java/com/watermelon/MyblogApplication.java
+++ myblog/src/main/java/com/watermelon/MyblogApplication.java
@@ -3,6 +3,8 @@ package com.watermelon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,11 +12,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages="com.watermelon.DAO")
 @ComponentScan({"com.watermelon.myblog","com.watermelon.service","com.watermelon.aspect"})
 @SpringBootApplication(scanBasePackages = "com.watermelon")
-public class MyblogApplication {
+public class MyblogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyblogApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 }
 
